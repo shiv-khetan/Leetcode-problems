a2bb376b33ca81c6bd7c219ffe74892ba6524c1e
Shivam Khetan <103183072+shiv-khetan@users.noreply.github.com>
2026-08-17 03:04:49 +0530

    Time: 2 ms (5.02%) | Memory: 8.6 MB (20.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-
-    int stepCount(int n,vector<int> &dp){
-        if(dp[n]!=-1)return dp[n];
-        return dp[n] = stepCount(n-1,dp) + stepCount(n-2,dp);
-    }
     int climbStairs(int n) {
         if(n<2)return 1;
         vector<int> dp(n+1,-1);
         dp[0]=1;
         dp[1]=1;
-        return stepCount(n,dp);
+
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
