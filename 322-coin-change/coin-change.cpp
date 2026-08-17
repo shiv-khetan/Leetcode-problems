423cdc3fd93d3c899a33fe63b389936b24be595e
Shivam Khetan <103183072+shiv-khetan@users.noreply.github.com>
2026-08-17 07:00:46 +0530

    Time: 43 ms (44.02%) | Memory: 44.4 MB (47.09%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, INT_MAX));
-
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 0;
-        }
+        vector<int> prev(amount + 1, INT_MAX);
+        prev[0] = 0;
 
         for (int amt = 1; amt < amount + 1; amt++){
             if(amt%coins[0]==0){
-                dp[0][amt]=amt/coins[0];
+                prev[amt]=amt/coins[0];
             }
         }
 
         for (int i = 1; i < n; i++) {
+            vector<int> curr(amount + 1, INT_MAX);
+            curr[0] = 0;
             for (int amt = 1; amt < amount + 1; amt++) {
-                dp[i][amt] = dp[i - 1][amt];
-                if (amt >= coins[i] && dp[i][amt - coins[i]] != INT_MAX)
-                    dp[i][amt] = min(dp[i][amt], 1 + dp[i][amt - coins[i]]);
+                curr[amt] = prev[amt];
+                if (amt >= coins[i] && curr[amt - coins[i]] != INT_MAX)
+                    curr[amt] = min(curr[amt], 1 + curr[amt - coins[i]]);
             }
+            prev=curr;
         }
-        return dp[n-1][amount]==INT_MAX?-1:dp[n-1][amount];
+        return prev[amount]==INT_MAX?-1:prev[amount];
     }
 };
