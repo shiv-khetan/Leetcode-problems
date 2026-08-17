beaea20c14b933aa1db4b901bd502164015c3ded
Shivam Khetan <103183072+shiv-khetan@users.noreply.github.com>
2026-08-17 19:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.09%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -11,7 +11,7 @@ public:
         return dp[idx] = max(take,notTake);
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
+        vector<int> dp(nums.size(),-1);
         return dfs(nums,0,dp);
     }
 };
